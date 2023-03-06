67a16a411297bf06b31a453d264332c0ebf684c6
Ahmed Awad <ahmedelsayed11595@gmail.com>
2023-03-05 21:59:26 -0600

    sprint 4

--- scripts/index.js
+++ scripts/index.js
@@ -25,17 +25,46 @@ let initialCards = [
   },
 ];
 
-let editButton = document.querySelector(".profile__edit-button");
-function open() {
-  let mod = document.querySelector(".modal");
-  mod.classList.add("modal_opened");
+let cardTempelate = document.querySelector("#card").content;
+let cardList = document.querySelector(".cards__list");
+
+for (let i = 0; i < initialCards.length; i++) {
+  let cardElement = cardTempelate.querySelector(".card").cloneNode(true);
+  let cardImage = cardElement.querySelector(".card__image");
+  let cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = initialCards[i].link;
+  cardImage.alt = initialCards[i].name;
+  cardTitle.textContent = initialCards[i].name;
+  cardList.append(cardElement);
 }
 
+let mod = document.querySelector(".modal");
+let editButton = document.querySelector(".profile__edit-button");
+let nameInput = document.querySelector("#name");
+let jobInput = document.querySelector("#job");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__description");
 let closeModal = document.querySelector(".modal__close-button");
+
+function open() {
+  mod.classList.add("modal_opened");
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
 function close() {
-  let mod = document.querySelector(".modal");
   mod.classList.remove("modal_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  close();
 }
 
 editButton.addEventListener("click", open);
 closeModal.addEventListener("click", close);
+mod.addEventListener("submit", handleProfileFormSubmit);
