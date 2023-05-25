b4d1cea0d7cfeb6a23e5e04020c3b2a88e9ffa7b
Ahmed Awad <ahmedelsayed11595@gmail.com>
2023-05-25 16:06:26 -0500

    Sprint-9

--- src/components/popupWithConfirmation.js
+++ src/components/popupWithConfirmation.js
@@ -1,6 +1,6 @@
 import Popup from "./Popup.js";
 
-export default class popupWithConfirmation extends Popup {
+export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
     this._confirmButton = this._modal.querySelector(".modal__save-button");
